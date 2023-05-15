60bf861664eb9f2ad238f843aeea3f68bf07b1c3
Benjamin Mellor <ben@smokingkangaroo.com>
2023-05-15 18:09:43 +1000

    initial commit

---
+++ devenv.nix
@@ -0,0 +1,31 @@
+{ pkgs, config, ... }:
+
+{
+  process.implementation = "overmind";
+
+  languages.php = {
+    enable = true;
+    version = "8.1";
+    ini = ''
+      memory_limit = 256M
+    '';
+    fpm.pools.web = {
+      settings = {
+        "pm" = "dynamic";
+        "pm.max_children" = 5;
+        "pm.start_servers" = 2;
+        "pm.min_spare_servers" = 1;
+        "pm.max_spare_servers" = 5;
+      };
+    };
+  };
+
+  services.caddy.enable = true;
+  services.caddy.virtualHosts."http://localhost:8000" = {
+    extraConfig = ''
+      root * public
+      php_fastcgi unix/${config.languages.php.fpm.pools.web.socket}
+      file_server
+    '';
+  };
+}
